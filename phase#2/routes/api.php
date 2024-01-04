a16642413dae1ac210f4c7873da75c8d8be5b0c0
MD SUJAN AHMED <csesujon155@gmail.com>
2024-01-04 10:09:33 +0600

    API Route Settings

--- phase#2/routes/api.php
+++ phase#2/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\APi\ProjectController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+/**
+ * Open API Route without any auth
+ */
+Route::controller(StudentController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login','login');
 });
+
+
+/**
+ * Protected API Route with Authentication
+ */
+Route::group([
+    "middleware" => ["auth:sanctum"],
+], function(){
+    // Student Controller
+    Route::get('profile', [StudentController::class, 'profile']);
+    Route::get('logout', [StudentController::class, 'logout']);
+
+    // Project Controller
+    Route::post('add-project', [ProjectController::class, 'addProject']);
+    Route::get('list-project', [ProjectController::class, 'getProjectList']);
+    Route::get('single-project/{id}', [ProjectController::class, 'getSingleProject']);
+    Route::delete('delete-project/{id}', [ProjectController::class, 'deleteProject']);
+});
+
