c7d37565aad1032384acf33636dbb03789db4759
MD SUJAN AHMED <csesujon155@gmail.com>
2024-01-04 09:08:35 +0600

    Student & Project Controllers and Methods Create

---
+++ phase#2/app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\APi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Add Project (POST, formdata)		--> Protected API Route
+     */
+    public function addProject(Request $request){
+
+    }
+
+    /**
+     * Get Project List By Student ID (GET)	--> Protected API Route
+     */
+    public function getProjectList(){
+
+    }
+
+    /**
+     * Get Single Project Details (GET)	--> Protected API Route
+     */
+    public function getSingleProject($project_id){
+
+    }
+
+    /**
+     * Delete Project (DELETE)		--> Protected API Route
+     */
+    public function deleteProject($project_id){
+
+    }
+}
---
+++ phase#2/app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Student Register (POST, formdata) 	--> Open API Route
+     */
+
+    public function register(Request $request){
+
+    }
+
+    /**
+     * Student Login (POST, formdata)	--> Open API Route
+     */
+    
+    public function login(Request $request){
+
+    }
+
+    /**
+     * Student Profile (GET)		--> Protected API Route
+     */
+
+    public function profile(){
+
+    }
+
+    /**
+     * Student Logout (GET)
+     */
+    public function logout(){
+        
+    }
+}
